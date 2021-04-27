fbf62648c2afa0538394b6c473a137739b528079
MCTzOCK <dev@mctzock.de>
2021-04-27 14:13:43 +0200

    Added a few more stuff

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const http      = require('http');
+const fs        = require('fs');
+const path      = require('path');
+
+
+
+let server = http.createServer((req, res) => {
+
+})
+
+server.listen(8080)
