30d53a7661b46a7af8e25e92cf5833d8c2ae998e
MCTzOCK <dev@mctzock.de>
2021-04-27 18:11:14 +0200

    added basic web server (default route)

--- src/routes/default.js
+++ src/routes/default.js
@@ -1,3 +1,32 @@
+const fs                = require('fs');
+const path              = require('path');
+const url               = require('url');
+const mime              = require('mime-types');
+const DOCUMENT_ROOT     = path.join(__dirname, '../public');
+
 exports.execute = function(req, res){
-    res.end('default')
+    let p = url.parse(req.url).path;
+    if(fs.existsSync(path.join(DOCUMENT_ROOT, p))){
+        if(!fs.lstatSync(path.join(DOCUMENT_ROOT, p)).isDirectory()){
+            res.writeHead(200, {"Content-Type": mime.lookup(p.split('/')[p.split('/').length])})
+            res.end(fs.readFileSync(path.join(DOCUMENT_ROOT, p)));
+        }else {
+            if(fs.existsSync(path.join(path.join(DOCUMENT_ROOT, p), 'index.html'))){
+                res.writeHead(200, {"Content-Type": mime.lookup(p.split('/')[p.split('/').length] + 'index.html')})
+                res.end(fs.readFileSync(path.join(path.join(DOCUMENT_ROOT, p), 'index.html')));
+            }else {
+                res.writeHead(404, {"Content-Type": "text/html"})
+                res.end(fs.readFileSync(path.join(__dirname, '../error/404.html')))
+            }
+        }
+    }else {
+        let t = path.join(DOCUMENT_ROOT, p) + '.html';
+        if(!fs.existsSync(t)){
+            res.writeHead(404, {"Content-Type": "text/html"})
+            res.end(fs.readFileSync(path.join(__dirname, '../error/404.html')))
+        }else {
+            res.writeHead(200, {"Content-Type": mime.lookup(p.split('/')[p.split('/').length] + '.html')})
+            res.end(fs.readFileSync(t));
+        }
+    }
 }
