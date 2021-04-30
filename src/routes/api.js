d1a2545434bc3dd27f05223fbfef6b6bd84bc42b
MCTzOCK <dev@mctzock.de>
2021-04-30 10:45:38 +0200

    added api token engine

--- src/routes/api.js
+++ src/routes/api.js
@@ -7,8 +7,12 @@ const hash  = require('../utils/hash')
 if(!fs.existsSync(path.join(__dirname, '../../config/users.json'))){
     fs.writeFileSync(path.join(__dirname, '../../config/users.json'), JSON.stringify({currentID: 0, users:[]}))
 }
+if(!fs.existsSync(path.join(__dirname, '../../config/tokens.json'))){
+    fs.writeFileSync(path.join(__dirname, '../../config/tokens.json'), JSON.stringify({}))
+}
 
 const uConf = JSON.parse(fs.readFileSync(path.join(__dirname, '../../config/users.json')))
+const tConf = JSON.parse(fs.readFileSync(path.join(__dirname, '../../config/tokens.json')));
 
 exports.execute = function (req, res){
     let p = url.parse(req.url).pathname;
@@ -51,15 +55,68 @@ exports.execute = function (req, res){
                         res.end(JSON.stringify({error: false, code: 200, msg: 'Successful'}));
                     }else {
                         res.writeHead(403, {'Content-Type': 'application/json'})
-                        res.end(JSON.stringify({error: true, code: 403, msg: 'Wrong username or password'}));
+                        res.end(JSON.stringify({error: true, code: 403, msg: 'Forbidden'}));
                     }
                 }
             })
             break;
-        case "registerapplication":
+        case "createtoken":
+            var userName        = a[3];
+            var password        = hash.toMD5(a[4]);
+            var allow           = true;
+            uConf['users'].forEach(user => {
+                if(user.name === userName){
+                    if(user.pass !== password){
+                        allow = false;
+                        res.writeHead(403, {'Content-Type': 'application/json'})
+                        res.end(JSON.stringify({error: true, code: 403, msg: 'Wrong username or password'}));
+                    }
+                }
+            })
+            if(allow){
+                let found = false;
+                let token = '';
+                while (!found){
+                    token = getRandomString(64);
+                    if(!tConf[token]){
+                        found = true;
+                    }
+                }
+                tConf[token] = userName;
+                fs.writeFileSync(path.join(__dirname, '../../config/tokens.json'), JSON.stringify(tConf))
+                res.writeHead(200, {'Conent-Type': 'application/json'});
+                res.end(JSON.stringify({error: false, code: 200, msg: 'Successful.', token: token}))
+            }
+            break;
+        case "getdata":
+            var token = a[3];
+            var data = a[4];
+            if(tConf[token]){
+                if(data === 'name'){
+                    res.writeHead(200, {'Content-Type': 'application/json'});
+                    res.end(JSON.stringify({error: false, code: 200, msg: 'Successful', data: tConf[token]}))
+                }else {
+                    res.writeHead(404, {'Content-Type': 'application/json'});
+                    res.end(JSON.stringify({error: true, code: 404, msg: 'Could not find data entry'}))
+                }
+            }else {
+                res.writeHead(403, {'Content-Type': 'application/json'})
+                res.end(JSON.stringify({error: true, code: 403, msg: 'Forbidden'}));
+            }
             break;
         default:
             res.writeHead(404, {'Content-Type': 'application/json'});
             res.end(JSON.stringify({error: true, code: 404, msg: 'Could not find API endpoint ' + a[2]}))
     }
+}
+
+function getRandomString(length) {
+    var result           = [];
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+        result.push(characters.charAt(Math.floor(Math.random() *
+            charactersLength)));
+    }
+    return result.join('');
 }
