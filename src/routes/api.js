166de3d07657de939c91b6ae95399a1619a3ea27
MCTzOCK <dev@mctzock.de>
2021-04-30 09:04:37 +0200

    Added api endpoint (user login)

--- src/routes/api.js
+++ src/routes/api.js
@@ -17,8 +17,8 @@ exports.execute = function (req, res){
     // TODO: Try Catch block
     switch(a[2].toLowerCase()){
         case "createuser":
-            let userName = a[3];
-            let password = hash.toMD5(a[4]);
+            var userName = a[3];
+            var password = hash.toMD5(a[4]);
             let allowCreation = true;
             uConf['users'].forEach(user => {
                 if(user.name === userName){
@@ -40,6 +40,23 @@ exports.execute = function (req, res){
                 res.writeHead(409, {'Content-Type': 'application/json'});
                 res.end(JSON.stringify({error: true, code: 409, msg: 'A user with that name does already exists.'}))
             }
+            break;
+        case "login":
+            var userName = a[3];
+            var password = hash.toMD5(a[4]);
+            uConf['users'].forEach(user => {
+                if(user.name === userName){
+                    if(user.pass === password){
+                        res.writeHead(200, {'Content-Type': 'application/json'})
+                        res.end(JSON.stringify({error: false, code: 200, msg: 'Successful'}));
+                    }else {
+                        res.writeHead(403, {'Content-Type': 'application/json'})
+                        res.end(JSON.stringify({error: true, code: 403, msg: 'Wrong username or password'}));
+                    }
+                }
+            })
+            break;
+        case "registerapplication":
             break;
         default:
             res.writeHead(404, {'Content-Type': 'application/json'});
